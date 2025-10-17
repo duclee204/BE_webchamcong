acf75ab62aebeaa53b28aeb745da6b5839d235b5
duclee204 <BIT220032@st.cmcu.edu.vn>
2025-10-17 10:38:20 +0700

    first commit

---
+++ src/main/java/org/example/webchamcongbe/WebchamcongBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.webchamcongbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebchamcongBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebchamcongBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/webchamcongbe/WebchamcongBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webchamcongbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchamcongBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
